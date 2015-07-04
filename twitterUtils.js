d701f0fb26d6c3b7204ca4dfa4fd66d9dc5bb9e9
Kyle Boss <kyle.boss@tubemogul.com>
2015-07-04 11:40:29 -0700

    removed getPreviousTweet

--- twitterUtils.js
+++ twitterUtils.js
@@ -1,6 +1,9 @@
-var twitter = module.exports = {}
-twitter.getPreviousTweet = function(tweet) {
-	T.get('statuses/user_timeline', { user_id: tweet["user"]["id"], count: 2 }, function(err, data, response) {
-  		console.log(data)
-	})
+module.exports = function(T) {
+	var twitter = {}
+	twitter.getPreviousTweet = function(tweet) {
+		T.get('statuses/user_timeline', { user_id: tweet["user"]["id"], count: 2 }, function(err, data, response) {
+	  		console.log(data)
+		})
+	}
+	return twitter
 }
