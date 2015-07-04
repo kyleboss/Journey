b0bb5955b7d5e0621023092e1bf936898e330804
Kyle Boss <kyle.boss@tubemogul.com>
2015-07-04 11:55:44 -0700

    Changing up exports format

--- twitterUtils.js
+++ twitterUtils.js
@@ -2,7 +2,7 @@ module.exports = function(T) {
 	var twitter = {}
 	twitter.getPreviousTweet = function(tweet) {
 		T.get('statuses/user_timeline', { user_id: tweet["user"]["id"], count: 2 }, function(err, data, response) {
-	  		console.log(data[0]["text"] + "\n" + data[1]["text"])
+	  		console.log(data[0]["text"] + "\n**" + data[1]["text"])
 	  		console.log("\n\n\n\n")
 		})
 	}
