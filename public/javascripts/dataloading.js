91a67b8ea4df7cede1ad3d8e0abf9384a08d2d32
Kyle Boss <kyle.boss@tubemogul.com>
2015-07-12 15:58:36 -0700

    Fixing new 404s

--- public/javascripts/dataloading.js
+++ public/javascripts/dataloading.js
@@ -21,7 +21,7 @@ function loadWorldPins( callback ){
 }
 
 function loadContentData(callback){	
-	var filePath = "categories/All.json";
+	var filePath = "javascripts/categories/All.json";
 	filePath = encodeURI( filePath );
 	// console.log(filePath);
 			
