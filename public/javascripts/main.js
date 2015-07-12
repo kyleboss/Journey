0050110ad5130e6d29cd290a89266d88f32905e4
Kyle Boss <kyle.boss@tubemogul.com>
2015-07-12 15:56:57 -0700

    Fixing new 404s

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,8 +9,8 @@ var mapOutlineImage;
 var glContainer = document.getElementById( 'glContainer' );
 
 //	contains a list of country codes with their matching country names
-var isoFile = 'country_iso3166.json';
-var latlonFile = 'country_lat_lon.json';
+var isoFile = 'javascripts/country_iso3166.json';
+var latlonFile = 'javascripts/country_lat_lon.json';
 
 var camera, scene, renderer, controls;
 
