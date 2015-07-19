0c07feb4cee9fd7c87168eac433d1aed1320b3c5
Kyle Boss <kyle.boss@tubemogul.com>
2015-07-18 22:39:51 -0700

    Formatting tweets so that map API can communicate

--- models/Tweet.js
+++ models/Tweet.js
@@ -52,7 +52,7 @@ schema.statics.getTweetsFromDb = function(callback) {
         // Pass them back to the specified callback
         tweets.forEach(function(tweet){
             console.log(tweet);
-            tweet.v = 3000000; // Set them to active
+            tweet["v"] = 3000000; // Set them to active
             console.log(tweet)
 
         });
