fadac83e18417ad479b5b0118b9d9e54178ee95f
Kyle Boss <kyle.boss@tubemogul.com>
2015-07-18 22:20:43 -0700

    Formatting tweets so that map API can communicate

--- models/Tweet.js
+++ models/Tweet.js
@@ -55,6 +55,7 @@ schema.statics.getTweetsFromDb = function(callback) {
             tweets[i]["v"] = 3000000;
             formattedTweets["timeBins"][0]["data"].push(tweets[i]);
         }
+        console.log(formattedTweets);
         callback(formattedTweets);
 
     });
