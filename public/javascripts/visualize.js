28b848b5938d3de1c9f93b8cd7bffbc6ac7e2657
Kyle Boss <kyle.boss@tubemogul.com>
2015-07-18 19:18:52 -0700

    Debugging particle mystery

--- public/javascripts/visualize.js
+++ public/javascripts/visualize.js
@@ -120,12 +120,13 @@ function getVisualizedMesh( linearData, year, countries, exportCategories, impor
 
 			var particleColor = lastColor.clone();		
 			var points = set.lineGeometry.vertices;
-			var particleCount = Math.floor(set.v / 8000 / set.lineGeometry.vertices.length) + 1;
+			//var particleCount = Math.floor(set.v / 8000 / set.lineGeometry.vertices.length) + 1;
+            var particleCount = 5;
+            var particleSize = 5;
 			particleCount = constrain(particleCount,1,100);
-			var particleSize = set.lineGeometry.size;
+			//var particleSize = set.lineGeometry.size;
 			console.log(particleSize)
             console.log(particleCount)
-            console.log(set.v);
             console.log("PARTICLES")
 			for( var s=0; s<particleCount; s++ ){
 				console.log("parcitle");
